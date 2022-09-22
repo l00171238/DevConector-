4630965006a2abe4610d11e2a0c701c085bee4b2
Manav Nim <l00171238@aatu.ie>
2022-09-22 06:01:33 -0700

    this push is from the lab

--- N1.js
+++ N1.js
@@ -1,4 +1,11 @@
 // JavaScript source 
+
+console.log("Astro  world !! ")
+
+console.log("Astro  world !! ")
+
+console.log("Astro  world !! ")
+
 console.log("Astro  world !! ")
 var mgs = "manav";
 console.log(mgs);
