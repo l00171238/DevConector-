7ab1eacee45cdd19ee024df4c5efeefaebc18537
manav nim <l00171238@aatu.ie>
2022-09-27 11:10:56 -0700

    Server running on port number 50000

---
+++ server.js
@@ -0,0 +1,6 @@
+const  express = require('express')
+const app = express();
+const port =  process.env.PORT || 5000
+
+app.get('/', (req, res) => res.send('API Running !'));
+app.listen(port, () => console.log(` Server listening on port ${port}!`));
